712519e701addcebb9b623f91bacca8b0114cda5
Neil Piper <n_piper@hotmail.com>
2016-10-05 00:55:29 +1100

    Added role ecsServiceRole

--- setup.sh
+++ setup.sh
@@ -18,4 +18,8 @@ aws iam create-role --role-name ecsInstanceRole --assume-role-policy-document fi
 # 5) Attach the policy 'AmazonEC2ContainerServiceforEC2Role' to 'ecsInstanceRole'
 aws iam attach-role-policy --role-name ecsInstanceRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role
 
- 
+# 6) Create the 'ecsServiceRole' for managing container instances / loadbalancing
+aws iam create-role --role-name ecsServiceRole --assume-role-policy-document file://ecsInstanceRole_Policy.json
+
+# 7) Attach the policy 'AmazonEC2ContainerServiceRole' to ecsServiceRole
+aws iam attach-role-policy --role-name ecsServiceRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole 
--- teardown.sh
+++ teardown.sh
@@ -3,8 +3,13 @@
 # Detatch role policies
 aws iam detach-role-policy --role-name ecsInstanceRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role 
 
+aws iam detach-role-policy --role-name ecsServiceRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole 
+
+
 # Delete role ecsInstanceRole
 aws iam delete-role --role-name ecsInstanceRole
+aws iam delete-role --role-name ecsServiceRole
+
 
 # Remove users from ECSAdmins
 aws iam remove-user-from-group --group-name ECSAdmins --user-name neil
