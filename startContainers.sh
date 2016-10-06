25bfda58243302db4d7d929ced74e98a4e07c39c
npiper <n_piper@hotmail.com>
2016-10-07 00:29:54 +1100

    update to containers

--- startContainers.sh
+++ startContainers.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Create cluster
+aws ecs create-cluster
+
 # Register task defintion
 aws ecs register-task-definition --cli-input-json file://simple-app-task-def.json
+
+# Create service - latest version of task family 'console-sample-app'
+aws ecs create-service --service-name ecs-simple-service --task-definition console-sample-app --desired-count 1
